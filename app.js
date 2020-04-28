5b913235bdb39debc1ffc6268e3385114ee8e06e
Amir Sohel <amirsohel@Amirs-MBP.lan>
2020-04-28 12:37:02 +0530

    enable cros for all origin

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express();
 const routes = require('./router/router')
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+//enable dotenv environment file
 require('dotenv').config()
+
+//enable cros for all origin
+app.use(cors())
 
 // parse application/json
 app.use(bodyParser.json())
@@ -10,26 +16,6 @@ app.use(bodyParser.json())
 //PORT 
 const PORT = process.env.PORT || 8000
 // Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-
 
 app.use('/', routes)
 
